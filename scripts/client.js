1f048505d6f2766ef390c13ebeb24fbfa09c3f1b
Dev Jana <devjana@Devs-MacBook-Pro-2.local>
2019-06-21 11:15:52 -0500

    basic logic working for add and display

--- scripts/client.js
+++ scripts/client.js
@@ -1,5 +1,7 @@
 $( document ).ready( onReady );
 
+let inventory = [];
+
 function addItem(){
     console.log( 'in addItem' );
     // capture user input
@@ -11,10 +13,24 @@ function addItem(){
     }
     console.log( 'adding:', newItem );
     // push object into an array
+    inventory.push( newItem );
     // display inventory
+    showInventory();
 }
 
 function onReady(){
     console.log( 'JQ' );
     $( '#addItemButton' ).on( 'click', addItem );
+}
+
+function showInventory(){
+    console.log( 'in showInventory' );
+    let el = $( '#inventoryOut' );
+    // empty our output element
+    el.empty();
+    // loop through the inventory
+    for( let i=0; i<inventory.length; i++ ){
+        // append each item to the DOM
+        el.append( `<li>${inventory[i].name}</li>` );
+    } //end for
 }
