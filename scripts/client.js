6e83311e9d4196a1c3875e28a6d5b11af3cdcd6a
Dev Jana <devjana@Devs-MacBook-Pro-2.local>
2019-06-21 11:10:16 -0500

    user input captured

--- scripts/client.js
+++ scripts/client.js
@@ -1,5 +1,20 @@
 $( document ).ready( onReady );
 
+function addItem(){
+    console.log( 'in addItem' );
+    // capture user input
+    // place in a new object
+    let newItem = {
+        color: $( '#colorIn' ).val(),
+        name: $( '#nameIn' ).val(),
+        size: $( '#sizeIn' ).val()
+    }
+    console.log( 'adding:', newItem );
+    // push object into an array
+    // display inventory
+}
+
 function onReady(){
     console.log( 'JQ' );
+    $( '#addItemButton' ).on( 'click', addItem );
 }
